50ecf2eab325a9427bd7321a6cb3938c04979550
Luiz Carlos K. Fukaya <luizcarloskazuyukifukaya@gmail.com>
2023-11-26 17:47:58 +0900

    Add placeholders with TODO

---
+++ waca_get_rgn_invoice_details.py
@@ -0,0 +1,7 @@
+# ======================================
+# Python WACA SDK
+# Python SDK with Wasabi Account Control API
+# No guaranty from Wasabi Inc.
+# ======================================
+
+# TODO: https://docs.wasabi.com/docs/get-details-for-a-sub-invoice
